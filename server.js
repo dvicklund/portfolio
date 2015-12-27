297e3358f7437e6f5b5dc7e5f4f8bbd1e9286ffd
David Vicklund <david@vicklund.com>
2015-12-27 04:39:31 -0800

    barebones server file

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var app = express();
-	
+
+app.use(express.static(__dirname + '/build'));
+
+var port = process.env.port || 3000;
+
+app.listen(port, function() {
+	console.log('server up on port: ' + port);
+})
