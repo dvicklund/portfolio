cc3c830a82b36e143fb0ae827dc3058da45f3084
David Vicklund <david@vicklund.com>
2015-12-27 05:32:06 -0800

    port reference fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/build'));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
 	console.log('server up on port: ' + port);
-})
+});
