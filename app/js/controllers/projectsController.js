0ab1a1ebb08ae7e3fea2ed8f61b64977b2bbc80c
David Vicklund <david@vicklund.com>
2016-10-02 00:37:47 -0700

    add project info

--- app/js/controllers/projectsController.js
+++ app/js/controllers/projectsController.js
@@ -1,5 +1,42 @@
 module.exports = function(app) {
 	app.controller('projectsController', ['$scope', function($scope) {
-    $scope.project = 'Something about productivity'
+		$scope.projects = [
+			{
+				name: 'CulinarySoft',
+				desc: 'An inventory management solution for chefs',
+				url: 'https://culinarysoft.herokuapp.com',
+				img: 'http://i.imgur.com/1Pz6a6w.png'
+			}, {
+				name: 'Photons Website',
+				desc: 'Band website for Seattle group Photons',
+				url: 'http://www.photons.rocks/',
+				img: 'http://i.imgur.com/HN9iuZL.png'
+			}, {
+				name: 'Where Not To Walk Your Dog',
+				desc: 'Interactive map of Department of Energy legacy maintenance sites',
+				url: 'https://wherenottowalkyourdog.herokuapp.com',
+				img: 'http://i.imgur.com/lyANBql.png'
+			}, {
+				name: 'Catch',
+				desc: 'Built for worried parents, a secure location tracker and alert system',
+				url: 'https://murmuring-springs-5499.herokuapp.com/',
+				img: 'http://i.imgur.com/4bKSszV.png'
+			}, {
+				name: 'Tiles For Days',
+				desc: 'Little game made over Thanksgiving weekend, 2015',
+				url: 'https://tilesfordays.herokuapp.com',
+				img: 'http://i.imgur.com/TOUhbH9.png'
+			}, {
+				name: 'FasterBids Account Viz',
+				desc: 'Data visualization suite for the proprietary FasterBids platform',
+				url: 'http://fasterbids.com/',
+				img: 'http://i.imgur.com/ChqUyFo.png'
+			}, {
+				name: 'Game of Life Clone',
+				desc: 'A tribute to the great Game of Life algorithm',
+				url: 'http://gameoflifedv.herokuapp.com',
+				img: 'http://i.imgur.com/uKEOi0l.png'
+			}
+		]
   }]);
 };
