6c78be6b3e553718e73a23b2edf539d02abdb6b8
David Vicklund <david@vicklund.com>
2022-08-04 16:00:59 -0700

    update gulpfile and pretty much everything else

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,40 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var webpack = require('webpack-stream');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var concatCss = require('gulp-concat-css');
-var sourcemaps = require('gulp-sourcemaps');
+const { src, dest, parallel } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const concatCss = require('gulp-concat-css')
+const minifyCss = require('gulp-minify-css')
+const webpack = require('webpack-stream')
+// const sourcemaps = require('gulp-sourcemaps')
 
-var paths = {
-  css: ['app/**/*.scss'],
-  html: ['app/**/*.html'],
-  js: ['app/**/*.js'],
-  test: ['test/testRoutes.js']
-};
+const paths = {
+    css: ['app/**/*.scss'],
+    html: ['app/**/*.html'],
+    js: ['app/**/*.js'],
+    jsEntry: ['app/js/entry.js'],
+    jsStarfield: ['app/js/starfield.js'],
+    test: ['test/testRoutes.js'],
+}
 
-gulp.task('build:css', function() {
-  gulp.src('app/scss/application.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(gulp.dest('app/css/'))
-    .pipe(concatCss('style.min.css'))
-    .pipe(minifyCss())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/'));
-});
+function css() {
+    return src(paths.css, {sourcemaps: true})
+        .pipe(sass())
+        .pipe(dest('app/css/'))
+        .pipe(concatCss('style.min.css'))
+        .pipe(minifyCss())
+        .pipe(dest('build/'), {sourcemaps: true})
+}
 
-gulp.task('build:html', function() {
-  gulp.src('app/**/*.html')
-  .pipe(gulp.dest('build/'));
-});
+function html() {
+    return src(paths.html)
+        .pipe(dest('build/'))
+}
 
-gulp.task('build:js', function() {
-  gulp.src('app/js/starfield.js').pipe(gulp.dest('build/'))
+function js() {
+    src(paths.jsStarfield).pipe(dest('build/'))
 
-  return gulp.src('app/js/entry.js')
-  .pipe(webpack({
-    output: {
-      filename: 'bundle.js'
-    }
-  }))
-  .pipe(gulp.dest('build/'));
-});
+    return src(paths.jsEntry)
+        .pipe(webpack({output: {filename: 'bundle.js'}}))
+        .pipe(dest('build/'))
+        
+}
 
-gulp.task('test:mocha', function() {
-	return gulp.src(paths.test).pipe(mocha({reporter: 'nyan'}));
-});
-
-gulp.task('watch:css', function() {
-	gulp.watch(paths.css, ['build:css']);
-});
-
-gulp.task('watch:html', function() {
-	gulp.watch(paths.html, ['build:html']);
-});
-
-gulp.task('watch:js', function() {
-	gulp.watch(paths.js, ['build:js']);
-});
-
-gulp.task('build:all', ['build:css', 'build:html', 'build:js']);
-gulp.task('test:all', ['test:mocha']);
-gulp.task('watch:all', ['watch:css', 'watch:html', 'watch:js']);
-gulp.task('default', ['build:all', 'watch:all']);
+exports.default = parallel(css, html, js);
