728e8daceca22beeb29d6de6d2bfc8b68ad099cc
David Vicklund <david@vicklund.com>
2023-01-15 22:57:47 -0800

    8080 back to 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/build'));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
 	console.log('server up on port: ' + port);
